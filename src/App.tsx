465885e1a2ef9bebac95db5e686a9bf686459bb1
Daniel <dwgrossberg@gmail.com>
2022-10-24 07:59:36 -0400

    Restructure project and add favicon

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App">Coming Soon</div>;
+}
+
+export default App;

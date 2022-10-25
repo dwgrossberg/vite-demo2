bf21462c1f8bdb15ff99898a32b0eec8cd0b7bcc
Daniel <dwgrossberg@gmail.com>
2022-10-25 07:25:40 -0400

    Remove dist

--- deploy.sh
+++ deploy.sh
@@ -21,9 +21,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git add dist -f
-git commit -m "Adding dist"
-git subtree push --prefix dist origin gh-pages
+git push -f git@github.com:dwgrossberg/dwgrossberg.github.io.git main:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages

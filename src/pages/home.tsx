ad4d18e4b5cb2818deb2810c107607af6e553520
Daniel <dwgrossberg@gmail.com>
2022-10-25 12:20:32 -0400

    Add code branch

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,5 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import { JSX as _jsx } from "@emotion/react/jsx-runtime";
 
 const home = () => {
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import vitePluginFaviconsInject from "vite-plugin-favicons-inject";
+// import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
-  plugins: [react(), vitePluginFaviconsInject("./src/assets/logo.png")],
+  plugins: [
+    react(),
+    vitePluginFaviconsInject("./src/assets/logo.png"),
+  ],
 });
